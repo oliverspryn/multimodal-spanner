e50cc2bf5a22b09d52be201ee4f4235c56037f9e
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2023-02-21 16:55:16 -0500

    Configure the Gradle version checker

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("com.android.application") version Versions.ANDROID_APPLICATION_PLUGIN apply false
     id("com.android.library") version Versions.ANDROID_LIBRARY_PLUGIN apply false
@@ -8,4 +10,22 @@ plugins {
 
 tasks.create<Delete>("clean") {
     delete(rootProject.buildDir)
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
+
+    checkForGradleUpdate = true
+    outputFormatter = "html"
+    outputDir = "build/dependencyUpdates"
+    reportfileName = "report"
 }
