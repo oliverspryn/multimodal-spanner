eba88bb1f1140e8fdc3e134e41032222047cb320
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2023-02-24 19:42:47 -0500

    Upgrade AGP, Gradle, dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,14 +14,15 @@ object Versions {
 
     // region Core Libraries
 
-    const val ANDROID_APPLICATION_PLUGIN = "7.2.2"
-    const val ANDROID_LIBRARY_PLUGIN = "7.2.2"
-    const val COMPOSE = "1.2.1"            // Reference this list for a the latest version:
-    const val COMPOSE_COMPILER = "1.3.1"   // https://developer.android.com/jetpack/androidx/releases/compose
-    const val DAGGER = "2.44"
-    val JVM = JavaVersion.VERSION_1_8
-    const val JVM_STRING = "1.8"
-    const val KOTLIN = "1.7.10"
+    const val ANDROID_APPLICATION_PLUGIN = "7.4.1"
+    const val ANDROID_LIBRARY_PLUGIN = "7.4.1"
+    const val COMPOSE = "1.3.3"            // Reference this list for a the latest version:
+    const val COMPOSE_COMPILER = "1.4.3"   // https://developer.android.com/jetpack/androidx/releases/compose
+    const val DAGGER = "2.45"
+    const val DOKKA = "1.7.20"
+    val JVM = JavaVersion.VERSION_11
+    const val JVM_STRING = "11"
+    const val KOTLIN = "1.8.10"
 
     // endregion
 
@@ -29,12 +30,12 @@ object Versions {
 
     // region Application
 
-    const val ACTIVITY_COMPOSE = "1.6.0"
+    const val ACTIVITY_COMPOSE = "1.6.1"
     const val CORE_KTX = "1.9.0"
-    const val MATERIAL = "1.8.0-alpha01"
-    const val MATERIAL_3 = "1.0.0-beta03"
-    const val NAVIGATION_COMPOSE = "2.6.0-alpha01"
-    const val WINDOW_MANAGER = "1.1.0-alpha03"
+    const val MATERIAL = "1.8.0"
+    const val MATERIAL_3 = "1.0.1"
+    const val NAVIGATION_COMPOSE = "2.5.3"
+    const val WINDOW_MANAGER = "1.0.0"
 
     // endregion
 
@@ -42,7 +43,7 @@ object Versions {
 
     // region Gradle tooling
 
-    const val GRADLE_PLUGIN_VERSIONS = "0.42.0"
+    const val GRADLE_PLUGIN_VERSIONS = "0.46.0"
 
     // endregion
 }
