755b47d79a368376fcdadc5a823a59078a04dc9b
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2024-03-28 15:46:20 -0400

    Exclude META-INF from compose

--- multimodal/build.gradle.kts
+++ multimodal/build.gradle.kts
@@ -18,6 +18,12 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = version("compose-compiler")
     }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
